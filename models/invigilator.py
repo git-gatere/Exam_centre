f09f9a9d8a4fa6cc4b0b5e03c980b4258e95a95b
gatere <mercynjerigatere@gmail.com>
2024-06-18 00:39:13 +0300

    Adds changes

--- models/invigilator.py
+++ models/invigilator.py
@@ -1,49 +1,52 @@
-#imports connection to database
 from database.connection import get_db_connection
 
-#creates a class Invigilator with proerties: id, name, e_mail and room
 class Invigilator:
-    def __init__(self, id,name, e_mail, room):
-        self.id = id
-        self.name = name
-        self.e_mail = e_mail
-        self.room = room
+    def __init__(self, id, name, e_mail, room):
+        self._id = id
+        self._name = name
+        self._e_mail = e_mail
+        self._room = room
 
     @property
     def id(self):
-        return self.id
+        return self._id
     
     @property
     def name(self):
-        return self.name
+        return self._name
 
     @property
     def e_mail(self):
-        return self.e_mail
+        return self._e_mail
     
     @property
     def room(self):
-        return self.room
+        return self._room
 
-# defines a function
     def save(self):
-        #calls the connection to the database
         conn = get_db_connection()
-        #creates a cursor
         cursor = conn.cursor()
-        #inserts the next row of values
-        cursor.execute('INSERT INTO invigilators (name, e_mail, room) VALUES (?, ?, ?)', (self.name, self.e_mail, self.room))
+        cursor.execute('INSERT INTO invigilators (name, e_mail, room) VALUES (?, ?, ?)', (self._name, self._e_mail, self._room))
         self._id = cursor.lastrowid 
         conn.commit()
         conn.close()
 
-
-# a class method that returns the whole invigilators table
     @classmethod
     def get_all(cls):
         conn = get_db_connection()
         cursor = conn.cursor()
         cursor.execute('SELECT * FROM invigilators')
-        invigilatorsrows = cursor.fetchall()
+        invigilators_rows = cursor.fetchall()
         conn.close()
-        return invigilatorsrows
+        return [cls(id=row[0], name=row[1], e_mail=row[2], room=row[3]) for row in invigilators_rows]
+
+    @classmethod
+    def get_by_id(cls, id):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM invigilators WHERE id = ?', (id,))
+        row = cursor.fetchone()
+        conn.close()
+        if row:
+            return cls(id=row[0], name=row[1], e_mail=row[2], room=row[3])
+        return None
